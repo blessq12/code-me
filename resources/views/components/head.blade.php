6583c627de906b7ed4b5527158dd95298d088d47
Andrew <code70@inbox.ru>
2023-04-28 13:22:30 +0700

    template add

---
+++ resources/views/components/head.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Ignite - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700" rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('/assets/css/open-iconic-bootstrap.min.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/animate.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/owl.carousel.min.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/owl.theme.default.min.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/magnific-popup.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/aos.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/ionicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/bootstrap-datepicker.css') }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/jquery.timepicker.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/flaticon.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/icomoon.css')  }}">
+    <link rel="stylesheet" href="{{ asset('/assets/css/style.css')  }}">
+  </head>
+  <body>
+    <!-- loader -->
+  <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+
+// Routes group below
+
+Route::domain(env('APP_URL'))->group(function(){
+    Route::controller(MainController::class)->group(function(){
+        Route::get('/','index');
+    });
 });
